94b55c3d0420223595d8b032d18925ec32d4c6c6
Giovanni Aratico <94821306+garatico@users.noreply.github.com>
2022-01-03 00:32:35 -0700

    Added custom HTML component for navbar with string builder

--- scripts/components.js
+++ scripts/components.js
@@ -1,93 +1,66 @@
 
-
-const navBarHTML = `<nav class="navbar navbar-expand-lg navbar-dark" id="navbar">
-<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navCollapse"
-    aria-controls="navCollapse" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-</button>
-<div class="collapse navbar-collapse" id="navCollapse">
-    <ul class="nav navbar-nav">
-        <li class="nav-item">
-            <!-- Returns user to home page -->
-            <a class="nav-link" href="index.html">
-                HOME
-            </a>
-        </li>
-        <li class="nav-item">
-            <!-- Takes user to music page -->
-            <a class="nav-link" href="./views/music.html">
-                MUSIC
-            </a>
-        </li>
-        <li class="nav-item">
-            <!-- Takes user to projects page -->
-            <a class="nav-link" href="./views/projects.html">
-                PROJECTS
-            </a>
-        </li>
-        <li class="nav-item">
-            <!-- Takes user to about page -->
-            <a class="nav-link" href="./views/about.html">
-                ABOUT
-            </a>
-        </li>
-    </ul>
-</div>
-</nav>`;
-
-const navBarHTMLSub = `<nav class="navbar navbar-expand-lg navbar-dark" id="navbar">
-<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navCollapse"
-    aria-controls="navCollapse" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-</button>
-<div class="collapse navbar-collapse" id="navCollapse">
-    <ul class="nav navbar-nav">
-        <li class="nav-item">
-            <!-- Returns user to home page -->
-            <a class="nav-link" href="../index.html">
-                HOME
-            </a>
-        </li>
-        <li class="nav-item">
-            <!-- Takes user to music page -->
-            <a class="nav-link" href="../views/music.html">
-                MUSIC
-            </a>
-        </li>
-        <li class="nav-item">
-            <!-- Takes user to projects page -->
-            <a class="nav-link" href="../views/projects.html">
-                PROJECTS
-            </a>
-        </li>
-        <li class="nav-item">
-            <!-- Takes user to about page -->
-            <a class="nav-link" href="../views/about.html">
-                ABOUT
-            </a>
-        </li>
-    </ul>
-</div>
-</nav>`;
-
-
+// Class allows for reusable components of the navbars on all pages
 class GiovanniNavBar extends HTMLElement {
+    // Constructor runs function to check page and pushes to custom component
     constructor() {
         super();
-        let whichNavBar = '';
-        whichNavBar = this.checkPage(this.getAttribute('page'));
+        let whichNavBar = this.navBuilder(this.getAttribute('page'));
         this.innerHTML = whichNavBar;
+    }
+    // Method builds custom Bootstrap NavBar depending on which page the user is on
+    navBuilder(attribute) {
+        let indexPage = "./index.html";
+        let music = "./views/music.html";
+        let project = "./views/projects.html";
+        let about = "./views/about.html";
+        let subPageCheck = this.checkPage(attribute);
+        let navBarHTML = `<nav class="navbar navbar-expand-lg navbar-dark" id="navbar">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navCollapse"
+        aria-controls="navCollapse" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navCollapse">
+        <ul class="nav navbar-nav">
+            <li class="nav-item">
+                <!-- Returns user to home page -->
+                <a class="nav-link" href=${subPageCheck + indexPage}>
+                    HOME
+                </a>
+            </li>
+            <li class="nav-item">
+                <!-- Takes user to music page -->
+                <a class="nav-link" href=${subPageCheck + music}>
+                    MUSIC
+                </a>
+            </li>
+            <li class="nav-item">
+                <!-- Takes user to projects page -->
+                <a class="nav-link" href=${subPageCheck + project}>
+                    PROJECTS
+                </a>
+            </li>
+            <li class="nav-item">
+                <!-- Takes user to about page -->
+                <a class="nav-link" href=${subPageCheck + about}>
+                    ABOUT
+                </a>
+            </li>
+        </ul>
+    </div>
+                    </nav>`;
+        return navBarHTML;
     }
 
     // Method checks which page of the website is shown
     checkPage(attribute) {
-        if (attribute == 'music' || attribute == 'about' || attribute == 'projects' ) {
-            return navBarHTMLSub;
+        if (attribute == 'music' || attribute == 'about' || attribute == 'projects') {
+            return ".";
         } else {
-            return navBarHTML;
+            return "";
         }
     }
 }
 
+// Defines custom HTML Element
 window.customElements.define('giovanni-navbar', GiovanniNavBar);
 

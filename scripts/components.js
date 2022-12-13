8c531357cb7876ecd54f696de9fdf9ddce01c6be
Giovanni Aratico <garatico@asu.edu>
2022-12-13 08:07:53 -0700

    Change portfolio projects section to cards

--- scripts/components.js
+++ scripts/components.js
@@ -16,48 +16,45 @@ class GiovanniNavBar extends HTMLElement {
     }
     // Method builds custom Bootstrap NavBar depending on which page the user is on
     navBuilder(attribute) {
-        let indexPage = "./index.html";
-        let music = "./views/music.html";
-        let project = "./views/projects.html";
-        let about = "./views/about.html";
+        let [indexPage, music, project, about] = ["./index.html", "./views/music.html", "./views/projects.html", "./views/about.html"];
         let subPageCheck = this.checkPage(attribute);
         let navBarHTML = `
         <div class="nav-container"> 
             <nav class="navbar navbar-expand-lg navbar-dark" id="navbar">
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navCollapse"
-        aria-controls="navCollapse" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navCollapse">
-        <ul class="nav navbar-nav">
-            <li class="nav-item">
-                <!-- Returns user to home page -->
-                <a class="nav-link" href=${subPageCheck + indexPage}>
-                    HOME
-                </a>
-            </li>
-            <li class="nav-item">
-                <!-- Takes user to music page -->
-                <a class="nav-link" href=${subPageCheck + music}>
-                    MUSIC
-                </a>
-            </li>
-            <li class="nav-item">
-                <!-- Takes user to projects page -->
-                <a class="nav-link" href=${subPageCheck + project}>
-                    PROJECTS
-                </a>
-            </li>
-            <li class="nav-item">
-                <!-- Takes user to about page -->
-                <a class="nav-link" href=${subPageCheck + about}>
-                    ABOUT
-                </a>
-            </li>
-        </ul>
-    </div>
-                    </nav>
-                    </div>`;
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navCollapse" aria-controls="navCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+            <div class="collapse navbar-collapse" id="navCollapse">
+                <ul class="nav navbar-nav">
+                    <li class="nav-item">
+                        <!-- Returns user to home page -->
+                        <a class="nav-link" href=${subPageCheck + indexPage}>
+                            HOME
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <!-- Takes user to projects page -->
+                        <a class="nav-link" href=${subPageCheck + project}>
+                            PROJECTS
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <!-- Takes user to music page -->
+                        <a class="nav-link" href=${subPageCheck + music}>
+                            MUSIC
+                        </a>
+                    </li>
+            
+                    <li class="nav-item">
+                        <!-- Takes user to about page -->
+                        <a class="nav-link" href=${subPageCheck + about}>
+                            ABOUT
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </div>`;
         return navBarHTML;
     }
 
